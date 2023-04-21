f968958059b1cd3c687b1241d303d2420f557c82
namle <namlgcd19254@fpt.edu.vn>
2023-04-21 22:12:42 +0700

    Tải ảnh file zip

--- FinalProject/Areas/Admin/Controllers/ExportDataController.cs
+++ FinalProject/Areas/Admin/Controllers/ExportDataController.cs
@@ -1,10 +1,13 @@
 ﻿using DocumentFormat.OpenXml.Bibliography;
 using DocumentFormat.OpenXml.Spreadsheet;
 using FinalProject.Models;
+using ICSharpCode.SharpZipLib.Zip;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -14,9 +17,11 @@ namespace FinalProject.Areas.Admin.Controllers
     public class ExportDataController : Controller
     {
         private readonly FinalProjectContext _context;
-        public ExportDataController(FinalProjectContext context)
+        private IHostingEnvironment _IHosting;
+        public ExportDataController(FinalProjectContext context, IHostingEnvironment iHosting)
         {
             _context = context;
+            _IHosting = iHosting;
         }
         public IActionResult ExportAllData ()
         {
@@ -31,6 +36,59 @@ namespace FinalProject.Areas.Admin.Controllers
             var data = Encoding.UTF8.GetBytes(sb.ToString());
             var result = Encoding.UTF8.GetPreamble().Concat(data).ToArray();
             return File(result, "text/csv", "Data.csv");
+        }
+        [Route("DownLoadZip.html", Name = "DownLoadZip")]
+        public FileResult DownLoadZip()
+        {
+            var webRoot = _IHosting.WebRootPath;
+            var fileName = "MyZip.zip";
+            var tempOutput = webRoot + "/images/" + fileName;
+
+            using (ZipOutputStream IzipOutputStream = new ZipOutputStream(System.IO.File.Create(tempOutput)))
+            {
+                IzipOutputStream.SetLevel(9);
+                byte[] buffer = new byte[4096];
+                var imageList = new List<string>();
+                var ideasURL = _context.Products.Where(x => x.Thumb != null).ToList();
+                foreach (var item in ideasURL)
+                {
+                    imageList.Add(webRoot + $"/images/products/{item.Thumb}");
+                }
+
+                for (int i = 0; i < imageList.Count; i++)
+                {
+                    ZipEntry entry = new ZipEntry(Path.GetFileName(imageList[i]));
+                    entry.DateTime = DateTime.Now;
+                    entry.IsUnicodeText = true;
+                    IzipOutputStream.PutNextEntry(entry);
+
+                    using (FileStream oFileStream = System.IO.File.OpenRead(imageList[i]))
+                    {
+                        int sourceBytes;
+                        do
+                        {
+                            sourceBytes = oFileStream.Read(buffer, 0, buffer.Length);
+                            IzipOutputStream.Write(buffer, 0, sourceBytes);
+                        } while (sourceBytes > 0);
+                    }
+                }
+                IzipOutputStream.Finish();
+                IzipOutputStream.Flush();
+                IzipOutputStream.Close();
+            }
+
+            byte[] finalResult = System.IO.File.ReadAllBytes(tempOutput);
+            if (System.IO.File.Exists(tempOutput))
+            {
+                System.IO.File.Delete(tempOutput);
+            }
+            if (finalResult == null || !finalResult.Any())
+            {
+                throw new Exception(String.Format("Nothing found"));
+
+            }
+
+            return File(finalResult, "application/zip", fileName);
         }
     }
 }
